e6377da80acf4595fdda9501937260f50fcb756e
Siddhartha Basu <biosidd@gmail.com>
2016-02-24 15:46:32 +0000

    feature:Added a basic navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+
+export class Navbar extends Component {
+    displayName = 'navbar component'
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="#">Page Editor</a>
+                    </div>
+                </div>
+            </nav>
+        )
+
+    }
+}
