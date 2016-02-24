239c1bec141b4b0e8dc8da7e9739f9bf591cf1ed
Siddhartha Basu <biosidd@gmail.com>
2016-02-24 12:14:55 +0000

    feature(router):Added routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,10 @@
-import {Route} from 'react-router';
+import {Route} from 'react-router'
+import Page from 'components/Page'
 
 const routes = (
-    /*
-     *write your routes here
-     *for example
-     *<Route path="/" component={ App }>
-     *    <Route path="login" component={ Login } />
-     *    <Route path="home" component={ Home } />
-     *    <Route path="error" component={ Error } />
-     *</Route>
-     */
+    <Route path="/" component={ App }>
+        <Route path=":page" component={ Page } />
+    </Route>
 );
 
-export default routes;
+export default routes
