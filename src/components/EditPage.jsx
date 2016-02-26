5f7e13c0bcccb0db0d4aa0f267d50009e6ec7a60
Siddhartha Basu <biosidd@gmail.com>
2016-02-26 15:35:22 +0000

    Added editor interface

--- src/components/EditPage.jsx
+++ src/components/EditPage.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
+import { Editor, EditorState } from 'draft-js'
+import 'styles/editor.scss'
+
 
 export default class EditPage extends Component {
     displayName = 'page editor'
+    constructor(props) {
+        super(props)
+        if (props.page.content) {
+            this.state = {
+                editorState:
+                    EditorState.createWithContent(props.page.content)
+            }
+        }
+    }
+    onChange = (editorState) => this.setState({editorState})
     render() {
-        const { routeProps } = this.props
+        const { editorState } = this.state
         return (
-          <h2>{ routeProps.routeParams.name }</h2>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-sm-offset-1 col-sm-7">
+                <div className="editor">
+                  <Editor
+                    editorState={ editorState }
+                    onChange={ this.onChange }
+                    ref="editor"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         )
     }
 }
