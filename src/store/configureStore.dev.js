b7957c9528b6507506d47d71a7a4c83b533a5d26
Siddhartha Basu <biosidd@gmail.com>
2016-02-26 15:45:48 +0000

    Added logger middleware

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,23 +1,24 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import rootReducer from '../reducers'
-import DevTools from '../containers/DevTools'
+import rootReducer from 'reducers'
+import DevTools from 'containers/DevTools'
+import createLogger from 'redux-logger'
 import { routerMiddleware } from 'react-router-redux'
 import { browserHistory } from 'react-router'
 
 const middleware = routerMiddleware(browserHistory)
 const enhancer = compose(
 // add middlewares as neccessary
-    applyMiddleware(middleware, thunk),
-    DevTools.instrument(),
+    applyMiddleware(middleware, thunk, createLogger()),
+    DevTools.instrument()
 )
 
 export default function configureStore(initialState) {
     const store = createStore(rootReducer, initialState, enhancer)
 
     if (module.hot) {
-        module.hot.accept('../reducers', () => {
-            store.replaceReducer(require('../reducers').default)
+        module.hot.accept('reducers', () => {
+            store.replaceReducer(require('reducers'))
         })
     }
     return store
