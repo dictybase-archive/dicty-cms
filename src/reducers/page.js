53cc876dd61ea3441cdac3199eaed5c75390ba96
Siddhartha Basu <biosidd@gmail.com>
2016-02-26 15:41:56 +0000

    Added comment about shape of the state

---
+++ src/reducers/page.js
@@ -0,0 +1,29 @@
+import types from 'constants'
+
+const { EDIT_PAGE } = types
+
+const initialState = {
+    content: null
+}
+
+const pageReducer = (state = initialState, action) => {
+    switch (action.type) {
+    case EDIT_PAGE:
+        return {
+            ...state,
+            content: action.payload.content
+        }
+    default:
+        return state
+    }
+}
+
+export default pageReducer
+
+/*
+ *shape of the state (update as neccessary)
+ *
+ *{
+ *    content: <ContentState object>
+ *}
+ */
