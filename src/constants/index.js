9c4d9f15d2f7edca62caf5341797cadd7b7bd1ce
Siddhartha Basu <biosidd@gmail.com>
2016-02-26 15:36:18 +0000

    feature:added constants for page management

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,8 @@
 // export your constants
-/*
- *const types = {
- *    LOGIN_REQUEST: 'LOGIN_REQUEST',
- *    LOGIN_SUCCESS: 'LOGIN_SUCCESS',
- *    LOGIN_FAILURE: 'LOGIN_FAILURE',
- *    LOGOUT_REQUEST: 'LOGOUT_REQUEST',
- *    LOGOUT_SUCCESS: 'LOGOUT_SUCCESS'
- *}
- */
-// export default types;
+const types = {
+    CREATE_PAGE: 'CREATE_PAGE',
+    READ_PAGE: 'READ_PAGE',
+    EDIT_PAGE: 'EDIT_PAGE',
+    DELETE_PAGE: 'DELETE_PAGE'
+}
+export default types
