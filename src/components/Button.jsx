d122430f19242d2b4bf8a5a0e4eb41397a014c60
Siddhartha Basu <biosidd@gmail.com>
2016-02-26 17:42:49 +0000

    refactor:Made it to a general purpose button

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import 'styles/bootstrap.scss'
+const Button = (props) => {
+    return (
+      <div className="row top5">
+        <div className="col-sm-offset-1 col-sm-4">
+          <button
+            onClick={ props.clickFn }
+            type="button"
+            className="btn btn-primary">
+            { props.label }
+          </button>
+        </div>
+      </div>
+    )
+}
+Button.displayName = 'simple buton'
+export default Button
