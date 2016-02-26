6317253be8c39f07ef08f066ba2e764c1f2c2da7
Siddhartha Basu <biosidd@gmail.com>
2016-02-26 15:43:21 +0000

    Removed the extra export for the component It should get exported by default

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,49 +2,25 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
-import * as actions from 'actions'
+import * as pageActionCreators from 'actions/page'
 import Navbar from 'components/Navbar'
 
 
 // Which part of the Redux global state does our component want to receive as props
 const mapStateToProps = (state, ownProps) => {
-     /*
-      *if the name of reducer is user then
-      *const { user } = state
-      *Then access state value through params like user.name etc.
-      *Look here https://github.com/gaearon/react-redux#quick-start
-      *
-      *The ownProps parameter allow to access router state in container component
-      *Look here https://github.com/reactjs/react-router-redux
-      */
-
-     /*
-      *Then return an object which would be available via props to dumb components
-      *   return { user: user, routeProps: ownProps }
-      */
-    return { routeProps: ownProps }
+    const { page } = state
+    return { routeProps: ownProps, page: page }
 }
 
-// Which action creators does it want to receive by props
-// Look here
-// https://github.com/gaearon/react-redux#connectmapstatetoprops-mapdispatchtoprops-mergeprops
 const mapDispatchToProps = (dispatch) => {
     return {
-        actions: bindActionCreators(actions, dispatch)
+        pageActions: bindActionCreators(pageActionCreators, dispatch)
     }
-    /*
-     *All actions creators(functions) will be available via this.props.actions
-     */
 }
 
-/*
- *Remember any other container component can implement both
- the mapper methods(or one of them) given above
- *get connected to redux store
- */
 
-export class App extends Component {
-    displayName = 'the primary app component';
+class App extends Component {
+    displayName = 'the primary app component'
     /*
      *Makes sure all the props passed to all the childrens to receive the state and actions
      */
@@ -55,11 +31,6 @@ export class App extends Component {
         })
     };
     render() {
-        /*
-         *It is recommended to select the dump components that are needed for the UI
-         *The selection could be done by inspecting various prop parameters that comes
-         *through redux store
-         */
         return (
           <div>
             <Navbar/>
